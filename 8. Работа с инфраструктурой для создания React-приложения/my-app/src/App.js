6f2be6e3c3eaa3640e34b2caf4eb66467686694d
Nikolai Eremin <nikolai_eremin@senla.eu>
2022-08-26 11:23:50 +0300

    8. Работа с инфраструктурой для создания React-приложения

---
+++ 8. Работа с инфраструктурой для создания React-приложения/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Итерация 7.</h1>
+        <h2>Работа с запросами с помощью JavaScript.</h2>
+        <input type="button" value="Выполни" onclick="UseAsyncAwait()"></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
